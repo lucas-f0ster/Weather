8702f712659a54b8ef10faef4716aa9784841e99
lucas-f0ster <lucasf284@gmail.com>
2021-07-13 12:22:04 +1200

    updated styling + anims for detail view + include more information

--- client/components/DetailedWeather.jsx
+++ client/components/DetailedWeather.jsx
@@ -1,28 +1,35 @@
 import React, { useState, useEffect } from 'react'
 
-const DetailedWeather = ({ weather, location }) => {
+const DetailedWeather = ({ weather, location, shown }) => {
+  const [selectedDay, setSelectedDay] = useState(null)
   const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
-  console.log(weather)
+  const handleClick = (index, date) => {
+    setSelectedDay(index === 0 ? 'Today' : days[index])
+  }
 
   return (
-    <>
-      <h1>{location}</h1>
+    <div className={`detailed${'__' + shown.toString()}`}>
+      <h1>{location || 'Your Location'}</h1>
+      <h2>Daily weather?</h2>
       <div className='detailed-daily'>
-        <h2>Daily weather?</h2>
         {weather.daily.map((e, i) => {
           if (i < 5) {
             const date = new Date(e.dt * 1000)
             return (
-              <div key={i} className='detailed-daily-info'>
-                <h4>{days[date.getDay()]}</h4>
+              <div key={i} className='detailed-daily-info' onClick={() => handleClick(i, date)}>
+                <h4>{i === 0 ? 'Today' : days[date.getDay()]}</h4>
+                <img src={`http://openweathermap.org/img/wn/${e.weather[0].icon}@2x.png`} />
+                <h4>High: <span className='temp__high'>{e.temp.max} &deg;C</span></h4>
+                <h4>Low: <span className='temp__low'>{e.temp.min} &deg;C</span></h4>
+                <h4>{e.weather[0].description}</h4>
               </div>
             )
           }
         })
         }
       </div>
-    </>
+    </div>
   )
 }
 
--- client/components/WeatherWidget.jsx
+++ client/components/WeatherWidget.jsx
@@ -32,17 +32,25 @@ const WeatherWidget = ({ weather, location }) => {
 
   return (
     <>
-      <div onClick={() => toggleWidget()} className={widgetExpanded ? 'weather-widget weather-widget__large' : 'weather-widget'}>
+      <div onClick={() => toggleWidget()}
+        className={`${!widgetWeather.alerts ? 'alert' : ''} 
+        ${widgetExpanded ? 'weather-widget weather-widget__large' : 'weather-widget'}`}>
         {widgetWeather.current && <>
-          {!widgetExpanded
-            ? <>
-              <h2>{widgetLocation || 'Your Location' }</h2>
-              <h3>{widgetWeather.current.temp}℃</h3>
-              <img src={`http://openweathermap.org/img/wn/${widgetWeather.current.weather[0].icon}@2x.png`} />
-              <h3>Currently {widgetWeather.current.weather[0].description} with {widgetWeather.current.humidity}% humidity</h3>
-              <h3>Feels like: {widgetWeather.current.feels_like}℃ </h3>
-            </>
-            : <DetailedWeather weather={widgetWeather} location={widgetLocation} />}
+          {!widgetExpanded && <>
+            <h2>{widgetLocation || 'Your Location' }</h2>
+            <h3>{widgetWeather.current.temp}&deg;C</h3>
+            <img src={`http://openweathermap.org/img/wn/${widgetWeather.current.weather[0].icon}@2x.png`} />
+            <h3>Currently {widgetWeather.current.weather[0].description} with {widgetWeather.current.humidity}% humidity</h3>
+            <h3>Feels like: {widgetWeather.current.feels_like}&deg;C </h3>
+            {!widgetWeather.alerts && <div className='alert__container'>
+              {/* <h3>{widgetWeather.alerts.event}</h3> */}
+              <h3>Wind warning</h3>
+            </div>}
+          </>}
+          <DetailedWeather
+            weather={widgetWeather}
+            location={widgetLocation}
+            shown={widgetExpanded} />
         </>}
       </div>
       {widgetExpanded && <div className='weather-widget weather-widget__placeholder'></div>}
